a1922e85b688095b7be7d2939ba0ae317839f674
volkanbasaran1 <volkanba12@gmail.com>
2024-02-24 15:34:13 +0300

    first commit

---
+++ src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import './Loader.scss';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="loader-square"></div>
+
+      <div className="loader-square"></div>
+
+      <div className="loader-square"></div>
+
+      <div className="loader-square"></div>
+
+      <div className="loader-square"></div>
+
+      <div className="loader-square"></div>
+
+      <div className="loader-square"></div>
+    </div>
+  );
+};
+
+export default Loader;
